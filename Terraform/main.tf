a6ec110be5e0860834e273c2b427f9f110f15828
EmanElshahat <eman221101037@outlook.com>
2026-02-12 18:44:36 +0200

    add

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -76,11 +76,23 @@ resource "aws_security_group" "app_sg" {
 }
 
 resource "aws_instance" "app_server" {
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
+
+  user_data = <<-EOF
+#!/bin/bash
+yum update -y
+yum install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+
+docker pull emanabosamra/app:latest
+docker run -d -p 80:5000 emanabosamra/app:latest
+EOF
 
   tags = {
     Name = "app-ec2"
