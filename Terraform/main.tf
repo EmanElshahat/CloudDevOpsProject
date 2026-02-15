5d116d0f9b367b804f599c4c658f35bce1fe1b38
EmanElshahat <eman221101037@outlook.com>
2026-02-15 17:11:16 +0200

    update terraform key

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -89,10 +89,15 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform-key"
+  public_key = file("terraform-key.pub")
+}
+
 resource "aws_instance" "app_server" {
   ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t3.medium"
-  key_name               = "terraform-key"
+  key_name               = aws_key_pair.terraform_key.key_name
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
